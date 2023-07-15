eea0b6cd7ca30eb4f4947da15612c7471cf47438
antonio.caballero.miro <antonio.caballero.miro@gmail.com>
2023-07-15 10:56:30 +0200

    Agregar archivos de proyecto.

---
+++ TwitchTextToVoice/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using TwitchTextToVoice.TwitchIntegration;
+
+namespace TwitchTextToVoice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Title = "Twitch text to voice";
+            Console.CursorVisible = false;
+
+            Console.WriteLine("Se va a abrir una ventana en el navegador para autorizar la aplicación.");
+            
+
+            TokenService tokenService = new TokenService();
+            if (tokenService.tokenEntity.error != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Ha ocurrido un error al autorizar la aplicación, ejecutala de nuevo para volver a intentarlo.");
+                Console.WriteLine();
+                Console.WriteLine("Pulsa cualquier tecla para salir.");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("**************************************************************************************************************");
+                Console.WriteLine();
+                Console.WriteLine("Bienvenido al conversor de texto a voz con integración del chat de Twitch.");
+                Console.WriteLine("Si se ha abierto una ventana en el navegador ya puedes cerrarla.");
+                Console.WriteLine();
+                Console.WriteLine("Seleccione una opción:");
+                Console.WriteLine("1.Empezar    2.Opciones    3.Salir");
+            }
+
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                if (input == '1')
+                {
+
+                }
+                else if (input == '2')
+                {
+                    Settings();
+                }
+                else if (input == '3')
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        static void Settings()
+        {
+            Console.Clear();
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("                                 AJUSTES");
+            Console.WriteLine();
+            Console.WriteLine("Introduce un número para cambiar el ajuste:");
+            Console.WriteLine();
+            Console.WriteLine("1.Nombre de usuario");
+            Console.WriteLine("2.Codigo de validación");
+        }
+
+        static void Application()
+        {
+
+        }
+    }
+}
---
+++ TwitchTextToVoice/TwitchIntegration/TokenService.cs
@@ -0,0 +1,115 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace TwitchTextToVoice.TwitchIntegration
+{
+    public class TokenService
+    {
+        string secret = "33ecm02di5du4ixb8fjugdpa5ifhy5";
+        string clientID = "e8cjlsdxanqiy2ckf2ucuffyfanz8s";
+        string baseurl = "https://id.twitch.tv/oauth2/authorize?";
+        string baseurlToken = "https://id.twitch.tv/oauth2/token";
+        public TokenResponseEntity tokenEntity;
+
+        public TokenService()
+        {
+            tokenEntity = GetUserToken();
+        }
+
+        private TokenResponseEntity GetUserToken()
+        {
+            string url = baseurl + "client_id=" + clientID +
+                "&redirect_uri=" + "http://localhost:3000/" +
+                "&response_type=" + "code" +
+                "&scope=" + "chat%3Aread" +
+                "&state=" + "0";
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:3000/");
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            listener.Start();
+            HttpListenerContext context = listener.GetContext();
+            listener.Close();
+            var error = context.Request.QueryString.Get("error");
+
+            if (error == null)
+            {
+                string code = context.Request.QueryString.Get("code") ?? "";
+                using (HttpClient httpclient = new HttpClient())
+                {
+                    List<KeyValuePair<string, string>> keyValuePairs = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("client_id", clientID),
+                        new KeyValuePair<string, string>("client_secret", secret),
+                        new KeyValuePair<string, string>("code", code),
+                        new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                        new KeyValuePair<string, string>("redirect_uri", "http://localhost:3000/")
+                    };
+                    HttpContent content = new FormUrlEncodedContent(keyValuePairs);
+                    var response = httpclient.PostAsync(baseurlToken, content).Result;
+                    var responseString = response.Content.ReadAsStringAsync().Result;
+                    var tokenObject = JsonSerializer.Deserialize<TokenResponseEntity>(responseString);
+                    if (tokenObject == null || tokenObject.access_token == null)
+                    {
+                        return new TokenResponseEntity { error = "Error no controlado" };
+                    }
+                    return tokenObject;
+                }
+            }
+            else
+            {
+                var result = new TokenResponseEntity
+                {
+                    error = error + " - " + (context.Request.QueryString.Get("error_description") ?? "").Replace('+', ' ')
+                };
+                return result;
+            }
+        }
+
+        public void RefreshToken()
+        {
+            using (HttpClient httpclient = new HttpClient())
+            {
+                List<KeyValuePair<string, string>> keyValuePairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("client_id", clientID),
+                    new KeyValuePair<string, string>("client_secret", secret),
+                    new KeyValuePair<string, string>("grant_type", "refresh_token"),
+                    new KeyValuePair<string, string>("refresh_token", UrlEncoder.Default.Encode(tokenEntity.refresh_token))
+                };
+                HttpContent content = new FormUrlEncodedContent(keyValuePairs);
+                var response = httpclient.PostAsync(baseurlToken, content).Result;
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                var result = JsonSerializer.Deserialize<TokenResponseEntity>(responseString);
+
+                tokenEntity = result;
+            }
+        }
+    }
+}
